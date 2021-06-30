41b833121cf830c4bb872dd7eabb02ecf41fa14b
Jonty Ward <jward0395@gmail.com>
2021-06-30 14:08:13 +0100

    commiting before doing more work

--- src/components/Interessts.js
+++ src/components/Interessts.js
@@ -10,7 +10,7 @@ const Interessts = () => {
       <div className="interests-container">
 
         {/* <h1>      This should contain some information about what i like to do for fun! </h1> */}
-        <p className="interests-paragraph">In my free time I love getting outdoors! Being out in the fresh air is hugely beneficial for my mental health. I have a love for all animals, and they play a big part in my life, whether it is seing a hedgehog on the side of the road, going on a holiday abroad to see and seeing exotic animals in the wild, or taking my (adorably daft-see photo of himn stuck in a style!) labrador out for his walks!
+        <p className="interests-paragraph">In my free time I love getting outdoors! Being out in the fresh air is hugely beneficial for my mental health. I have a love for all animals, and they play a big part in my life, whether it is seing a hedgehog on the side of the road, going on a holiday abroad to see and seeing exotic animals in the wild, or taking my (adorably daft-see photo of him stuck in a style!) labrador out for his walks!
           <br/>
           <br/>
          I love to travel, and during the pandemic have taken the opportunity to travel more locally. However top of my list when we can travel more freely is Colombia, for its wildlife, culture, and history! 
