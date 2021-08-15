fe65e23cecf7f99be07ad54ee6e8832ceb99fda8
Jonty Ward <jward0395@gmail.com>
2021-08-15 20:41:05 +0100

    closer

---
+++ src/components/ScrollButton.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { FaArrowCircleDown } from 'react-icons/fa'
+import { Button } from './Styles'
+	
+const ScrollButton = () =>{
+	
+  const [visible, setVisible] = useState(true)
+	
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop
+    if (scrolled > 0){
+      setVisible(false)
+    } else if (scrolled <= 0){
+      setVisible(true)
+    }
+  }
+	
+  const scrollToBottom = () =>{
+
+    document.getElementById('aboutme').scrollIntoView()
+  }
+	
+  window.addEventListener('scroll', toggleVisible)
+	
+  return (
+    <Button>
+      <FaArrowCircleDown onClick={scrollToBottom}
+        style={{ display: visible ? 'inline' : 'none' }} />
+    </Button>
+  )
+}
+	
+export default ScrollButton
+
